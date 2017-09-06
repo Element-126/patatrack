963e18c915ddf9e13566a345f75997d8aea9af60
Jean-Loup Tastet <jean-loup.tastet@cern.ch>
2017-09-06 14:26:54 +0200

    Make Host::Quadruplet printable.

--- 05_ca/CellularAutomaton/HostQuadruplet.hpp
+++ 05_ca/CellularAutomaton/HostQuadruplet.hpp
@@ -1,8 +1,17 @@
 #pragma once
 
 #include <array>
+#include <iostream>
 
 namespace Host
 {
     using Quadruplet = std::array<std::array<int, 2>, 3>;
+}
+
+inline std::ostream & operator<<(std::ostream & os, Host::Quadruplet const &q)
+{
+    constexpr auto sep = ", ";
+    return os << q[0][0] << sep << q[0][1] << sep
+              << q[1][0] << sep << q[1][1] << sep
+              << q[2][0] << sep << q[2][1];
 }
